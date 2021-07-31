99b6f08a8fe5f0e218a4acfdf8295d4e17ff46d9
ernes <ernesto-na@outlook.com>
2021-07-31 03:52:38 +0200

    agregamos la practica 1

---
+++ Practica1/Cliente.h
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+class Cliente
+{
+      private:
+              string nombre,apellidoP, apellidoM;
+              int edad;
+              float montoCuenta;
+
+      public:
+             Cliente()
+             {
+             nombre="Sin nombre";
+             apellidoP="Sin apellido";
+             apellidoM="Sin apellido";
+             edad=18;
+             montoCuenta=0.0;
+             }
+    void setNombre(string nombre)
+             {
+                  this->nombre=nombre.length()<=30?nombre:"Error";
+             }
+    string getNombre()
+             {
+                    return nombre;
+             }
+
+    void setApellidoP(string apellidoP)
+             {
+                  this->apellidoP=apellidoP.length()<=25?apellidoP:"Error";
+             }
+    string getApellidoP()
+             {
+                    return apellidoP;
+             }
+    void setApellidoM(string apellidoM)
+             {
+                  this->apellidoM=apellidoM.length()<=25?apellidoM:"Error";
+             }
+    string getApellidoM()
+             {
+                    return apellidoM;
+             }
+    void setEdad(int edad)
+             {
+                  this->edad=edad>=0 && edad<=100?edad:-1;
+             }
+    int getEdad()
+             {
+                 return edad;
+             }
+    void setMontoCuenta(float montoCuenta)
+             {
+                  this->montoCuenta=montoCuenta>0.0?montoCuenta:0;
+             }
+    float getMontoCuenta()
+             {
+                   return montoCuenta;
+             }
+
+
+
+}
---
+++ Practica1/TestCliente.cpp
@@ -0,0 +1,12 @@
+
+#include"Cliente.h"
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+
+        int main()
+            {
+                cout<<"Escribe el numero del articulo que deseas quitar"<<endl;
+            }
