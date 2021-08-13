1aa121806c5501f5fea379ee858a17a260526205
ernesto-na@outlook.com <ernesto.nepomuceno.contractor@bbva.com>
2021-08-12 20:36:18 -0500

    agregamos main

--- Practica6/Test.cpp
+++ Practica6/Test.cpp
@@ -2,3 +2,14 @@
 #include"Cliente.h"
 #include <stdio.h>
 #include<iostream>
+#include <stdlib.h>
+#include <conio.h>
+using namespace std;
+
+        int main()
+            {
+                   cout<<"Hola persona";
+                   Persona persona;
+                   persona.leerPersona();
+                   persona.mostrarPersona();
+            }
